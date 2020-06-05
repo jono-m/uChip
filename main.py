c74060d9c0558757e4adc1047cdc3f8dc3086a30
Jono Matthews <jono.jmm@gmail.com>
2020-06-05 11:44:33 -0500

    Fixed some repo/venv issues.

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from PySide2 import QtWidgets
 from UI.MainWindow.MainWindow import *
-import faulthandler
 import sys
-# import cProfile
-
-#
-# sys.stdout = open("outLog.txt", "w")
-# sys.stderr = open("errLog.txt", "w")
-
-faulthandler.enable()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
 
-    # cProfile.run('app.exec_()', filename="dataOut.prof")
     app.exec_()
