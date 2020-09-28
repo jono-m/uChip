de757571b68036d8f7ee59bf8f27e6d26df72f76
Jono Matthews <jono.jmm@gmail.com>
2020-09-28 15:28:43 -0500

    .

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from cx_Freeze import setup, Executable
 setup(name="uChip",
       version="0.1",
       description="",
-      executables=[Executable("main.py")])
+      executables=[Executable(
+            script="main.py",
+            icon="Assets/icon.ico")])
