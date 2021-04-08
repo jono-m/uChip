e7019372a53a0c9d4e4012cec4cc1a173de4eaaf
Jonathan Matthews <jono.jmm@gmail.com>
2021-04-08 16:42:21 -0500

    .

---
+++ UI/ProjectEditor/BlockItem.py
@@ -0,0 +1,96 @@
+from UI.ProjectEditor.SelectableItem import *
+from UI.StylesheetLoader import *
+
+
+class BlockItem(QGraphicsProxyWidget, SelectableItem):
+    def __init__(self, scene: QGraphicsScene):
+        super().__init__()
+
+        self.blockWidget = QFrame()
+        self.blockWidget.setObjectName("BlockItem")
+        self.setWidget(self.blockWidget)
+        scene.addItem(self)
+
+        StylesheetLoader.GetInstance().RegisterWidget(self.blockWidget)
+
+        self.blockLayout = QVBoxLayout()
+        self.blockLayout.setContentsMargins(0, 0, 0, 0)
+        self.blockLayout.setSpacing(0)
+        self.blockWidget.setLayout(self.blockLayout)
+
+        self.container = QFrame()
+        self.blockLayout.addWidget(self.container)
+
+        self.displayHovered = False
+        self.displaySelected = False
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        self.container.setLayout(layout)
+
+        self.UpdateStyle()
+
+    def SetIsHovered(self, hoverOn):
+        if self.displayHovered != hoverOn:
+            self.displayHovered = hoverOn
+            self.UpdateStyle()
+
+    def SetIsSelected(self, isSelected):
+        if self.displaySelected != isSelected:
+            self.displaySelected = isSelected
+            self.UpdateStyle()
+
+    def UpdateStyle(self):
+        oldProperty = self.container.property('state')
+        if self.displaySelected:
+            if self.displayHovered:
+                self.container.setProperty('state', 'HoverAndSelect')
+            else:
+                self.container.setProperty('state', 'Select')
+        else:
+            if self.displayHovered:
+                self.container.setProperty('state', 'Hover')
+            else:
+                self.container.setProperty('state', 'None')
+        if oldProperty is None or oldProperty != self.container.property('state'):
+            self.container.setStyle(self.container.style())
+
+    def IsContainerSelected(self, scenePos):
+        childAt = self.blockWidget.childAt(self.mapFromScene(scenePos).toPoint())
+        if childAt is None or isinstance(childAt, QLabel) or isinstance(childAt, QFrame):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def IsChildFocused(widget: QWidget):
+        for child in widget.children():
+            if isinstance(child, QWidget):
+                if child.hasFocus() or BlockItem.IsChildFocused(child):
+                    return True
+        return False
+
+    @staticmethod
+    def ClearFocusAll(widget):
+        widget.clearFocus()
+        for child in widget.children():
+            if isinstance(child, QWidget):
+                BlockItem.ClearFocusAll(child)
+
+    def mousePressEvent(self, event: QGraphicsSceneMouseEvent):
+        self.ClearFocusAll(self.blockWidget)
+        super().mousePressEvent(event)
+
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent):
+        self.OnDoubleClick()
+        super().mousePressEvent(event)
+
+    def DoMove(self, currentPosition: QPointF, delta: QPointF):
+        self.setPos(self.pos() + delta)
+
+    def IsMovableAtPoint(self, scenePoint: QPointF):
+        return self.IsContainerSelected(scenePoint)
+
+    def OnDoubleClick(self):
+        pass
---
+++ UI/ProjectEditor/CustomLogicBlockEditorFrame.py
@@ -0,0 +1,61 @@
+from UI.ProjectEditor.BaseEditorFrame import *
+
+
+class CustomLogicBlockEditorFrame(BaseEditorFrame):
+    def __init__(self, logicBlock: CompoundLogicBlock):
+        super().__init__()
+
+        self.logicBlock = logicBlock
+
+        self.editor = LogicBlockEditor()
+        self.nameFilters = ["μChip Logic Block (*.ulb)"]
+
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        self.setLayout(layout)
+        layout.addWidget(self.editor)
+
+        self.logicBlock.OnModified.Register(self.FileModified, True)
+        self.logicBlock.OnSaved.Register(self.FileSaved, True)
+
+        self.editor.LoadBlock(self.logicBlock)
+
+        self.updateTimer = QTimer(self)
+        self.updateTimer.timeout.connect(self.TimerUpdate)
+        self.updateTimer.start(100)
+
+        self.hasFilename = logicBlock.absolutePath is not None
+
+    def TimerUpdate(self):
+        if self.logicBlock is not None:
+            self.logicBlock.ComputeOutputs()
+
+    def AddLogicBlock(self, lb: ConnectableBlock):
+        lb.SetPosition(self.editor.worldBrowser.GetCenterPoint())
+        if isinstance(lb, CompoundLogicBlock) and self.logicBlock.CreatesLoop(lb):
+            QMessageBox.warning(parent=self, title="Element addition failed",
+                                text="The current logic block is used in the logic block that you are trying to "
+                                     "add. This would create a never-ending loop! Cannot add.")
+        else:
+            self.logicBlock.AddSubBlock(lb)
+
+    def AddImage(self, image: Image):
+        self.logicBlock.AddImage(image)
+
+    def RequestClose(self):
+        if super().RequestClose():
+            self.logicBlock.OnModified.Unregister(self.FileModified)
+            self.logicBlock.OnModified.Unregister(self.FileSaved)
+            self.logicBlock.Destroy()
+            return True
+        return False
+
+    def DoSave(self, filename=None):
+        self.logicBlock.Save(filename)
+
+    def GetName(self) -> str:
+        return self.logicBlock.GetName()
+
+    def UpdateFromSave(self):
+        self.logicBlock.ReloadFileSubBlocks()
