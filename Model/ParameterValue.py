d753b9f7f7d01e112597cc9a5ba5be3b4cfa6290
Jonathan Matthews <jono.jmm@gmail.com>
2021-04-28 10:36:32 -0500

    .

--- Model/ParameterValue.py
+++ Model/ParameterValue.py
@@ -1,6 +1,28 @@
-from abc import ABC
+import typing
 
 
 class ParameterValue:
+    def __init__(self, initialValue):
+        self.value = initialValue
+
+
+class NumberValue(ParameterValue):
     def __init__(self):
-        self.value = None
+        super().__init__(0)
+
+        self.minimum = None
+        self.step = None
+        self.maximum = None
+
+        self.integersOnly = False
+
+
+class BooleanValue(ParameterValue):
+    def __init__(self):
+        super().__init__(False)
+
+
+class ListValue(ParameterValue):
+    def __init__(self):
+        super().__init__(0)
+        self.options: typing.List[str] = []
