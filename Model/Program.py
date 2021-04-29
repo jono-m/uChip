ac160a66753c6d05deeff71c991c4bed9d4e6399
Jono Matthews <jono.jmm@gmail.com>
2021-04-28 20:13:38 -0500

    .

--- Model/Program.py
+++ Model/Program.py
@@ -1,20 +1,58 @@
-from enum import Enum, auto
-from Parameter import Parameter
-from ParameterValue import ParameterValue
 import typing
+
+Parameters = typing.List[typing.Tuple[str, 'ParameterDescription']]
+ParameterValues = typing.List[typing.Tuple[str, 'ParameterValue']]
 
 
 class Program:
     def __init__(self):
-        self.parameters: typing.List[Parameter] = []
-
-    def Execute(self, parameterValues: typing.Dict['Parameter', 'ParameterValue']):
-        pass
+        self.name = "Unnamed Program"
+        self.parameters: Parameters = []
 
 
-class ScriptedProgram(Program):
+class ProgramInstance:
+    def __init__(self, program: Program):
+        self.program = program
+        self.parameterValues = 
+        self.isRunning = False
+
+
+class ProgramButton:
+    def __init__(self, program: Program):
+        self.programInstance = ProgramInstance(program)
+
+
+class ParameterDescription:
+    def __init__(self, initialDefaultValue):
+        self.defaultValue = initialDefaultValue
+
+
+class BooleanParameterDescription(ParameterDescription):
     def __init__(self):
-        super().__init__()
+        super().__init__(False)
 
-        # Signature: Parameters() -> List[]
-        self.parametersScript
+
+class NumberParameterDescription(ParameterDescription):
+    def __init__(self):
+        super().__init__(0)
+        self.minimum = None
+        self.maximum = None
+        self.step = None
+        self.isInteger = False
+
+
+class ValveParameterDescription(ParameterDescription):
+    def __init__(self):
+        super().__init__(None)
+
+
+class OptionParameterDescription(ParameterDescription):
+    def __init__(self):
+        super().__init__(0)
+        self.options = ["Option 1"]
+
+
+class ParameterValue:
+    def __init__(self, value, description: ParameterDescription):
+        self.value = value
+        self.description = description
