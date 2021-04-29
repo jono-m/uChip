3a36c947b45926360f59417bfe7822f11843912e
Jono Matthews <jono.jmm@gmail.com>
2021-04-28 20:13:38 -0500

    .

--- Model/Parameter.py
+++
@@ -1,8 +0,0 @@
-import typing
-from ParameterValue import ParameterValue
-
-
-class Parameter:
-    def __init__(self):
-        self.name = ""
-        self.valueType: typing.Optional[typing.Type[ParameterValue]] = None
--- Model/ProgramButton.py
+++
@@ -1,6 +0,0 @@
-class ProgramButton:
-    def __init__(self):
-        self.xPosition = 0
-        self.yPosition = 0
-        self.program = None
-        self.parameterValues = []
